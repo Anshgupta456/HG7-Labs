cbd8f8971559be9cd7156fa5a23e1203d1eab004
Anshgupta0910 <anshikagupta7.paawanigroup@gmail.com>
2025-08-17 10:39:06 +0530

    Hero Section Added

--- hgseven/src/App.jsx
+++ hgseven/src/App.jsx
@@ -1,10 +1,15 @@
 import Navbar from "./components/navbar/Navbar"
+import Hero from "./components/hero/hero"
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Hero />
+      <div className="mx-auto h-screen flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold">UNDER DEVELOPMENT</h1>
+      </div>
     </>
   )
 }
